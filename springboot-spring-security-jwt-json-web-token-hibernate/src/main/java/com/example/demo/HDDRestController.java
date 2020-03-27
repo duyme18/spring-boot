c063169e59d004b34acbf7086ab102c1771fe1d2
Duy <duyme18@gmail.com>
2020-03-27 17:33:28 +0700

    springboot-security-jwt-web-token

---
+++ springboot-spring-security-jwt-json-web-token-hibernate/src/main/java/com/example/demo/HDDRestController.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import javax.validation.Valid;
+
+import com.example.demo.jwt.JwtTokenProvider;
+import com.example.demo.payload.LoginRequest;
+import com.example.demo.payload.LoginResponse;
+import com.example.demo.payload.RandomStuff;
+import com.example.demo.user.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HDDRestController {
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @PostMapping("/login")
+    public LoginResponse authenticateUser(@Valid @RequestBody LoginRequest loginRequest){
+        // Xác thực từ username và password.
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+        // Nếu không xảy ra exception tức là thông tin hợp lệ
+        // Set thông tin authentication vào Security Context
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        // Trả về jwt cho người dùng.
+        String jwt = jwtTokenProvider.generateToken((CustomUserDetails) authentication.getPrincipal());
+        return new LoginResponse(jwt);
+    }
+
+    // Api /api/random yêu cầu phải xác thực mới có thể request
+    @GetMapping("/random")
+    public RandomStuff randomStuff(){
+        return new RandomStuff("JWT Hợp lệ mới có thể thấy được message này");
+    }
+}
